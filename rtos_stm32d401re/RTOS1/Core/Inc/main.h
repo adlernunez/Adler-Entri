dfbedcac3175d27e7d20e309f02d07e81922c724
Adler Nunez <adlernunez2001@gmail.com>
2026-07-03 12:42:46 +0530

    added 3 led project in main and commented out hello task project

--- rtos_stm32d401re/RTOS1/Core/Inc/main.h
+++ rtos_stm32d401re/RTOS1/Core/Inc/main.h
@@ -65,6 +65,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define red_led_ext_Pin GPIO_PIN_6
+#define red_led_ext_GPIO_Port GPIOA
+#define green_led_ext_Pin GPIO_PIN_7
+#define green_led_ext_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
